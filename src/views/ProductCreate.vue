97145e552ebe52c5062df4628cfa17802a713e1a
Artem Tsepkov <tzepart@gmail.com>
2020-04-21 02:28:32 +0300

    Create ProductCreate and ProductList pages + routing rules

---
+++ src/views/ProductCreate.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Product Create</h1>
+</template>
+
+<script>
+    export default {
+        name: "ProductCreate"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ProductList.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Product List</h1>
+</template>
+
+<script>
+    export default {
+        name: "ProductList"
+    }
+</script>
+
+<style scoped>
+
+</style>
