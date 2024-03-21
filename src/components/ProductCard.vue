32bd52b8d5dc4512d7a85e5ecf76d121677feaf7
Artem Tsepkov <tzepart@gmail.com>
2024-03-21 16:05:44 +0100

    add examples of methods

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,108 +1,77 @@
 <template>
     <div class="product">
         <div class="product-image">
-            <img :src="image" :alt="product"/>
+            <img v-bind:src="image" :alt="product"/>
         </div>
 
         <div class="product-info">
             <h1>{{ product }}</h1>
-            <p v-if="inventory>10">In Stock</p>
-            <p v-else-if="inventory<=10 && inventory>0">Almost sold out!</p>
-            <p v-else>Out of Stock</p>
+            <div v-show="isProductShow">
+                <p v-if="inventory>10">In Stock</p>
+                <p v-else-if="inventory<=10 && inventory>0">Almost sold out!</p>
+                <p v-else>Out of Stock</p>
+
+                <ul>
+                    <li v-for="detail in details" :key="detail.material">
+                        {{ detail.material }},
+                        {{ detail.add_material }},
+                        {{ detail.type }}
+                    </li>
+                </ul>
+
+                <div v-for="variant in variants" :key="variant.variantId">
+                    <p @mouseover="updateProduct(variant.variantImage)">{{ variant.variantColor }}</p>
+                </div>
+
+                <button @click="addToCart">Add to cart</button>
+
+                <div class="cart">
+                    <p>Cart({{ cart }})</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'ProductCard',
+        name: "ProductCard",
         data: function () {
             return {
-                product: 'Socks',
-                image: require('./../assets/images/vmSocks-green-onWhite.jpg'),
-                inventory: 5
+                product: "Socks",
+                image: require("./../assets/images/vmSocks-green-onWhite.jpg"),
+                inventory: 5,
+                isProductShow: true,
+                details: [{
+                    "material": "Cotton",
+                    "add_material": "Smth elso",
+                    "type": "Uni-sex",
+                }],
+                variants: [
+                    {
+                        variantId: 2234,
+                        variantColor: 'green',
+                        variantImage: require("./../assets/images/vmSocks-green-onWhite.jpg")
+                    },
+                    {
+                        variantId: 2235,
+                        variantColor: 'blue',
+                        variantImage: require("./../assets/images/vmSocks-blue-onWhite.jpg")
+                    }
+                ],
+                cart: 0
             };
+        },
+        methods: {
+            addToCart() {
+                this.cart += 1
+            },
+            updateProduct(variantImage) {
+                this.image = variantImage
+            }
         }
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-    .product {
-        display: flex;
-        flex-flow: wrap;
-        padding: 1rem;
-    }
-
-    img {
-        border: 1px solid #d8d8d8;
-        width: 70%;
-        margin: 40px;
-        box-shadow: 0px .5px 1px #d8d8d8;
-    }
-
-    .product-image {
-        width: 80%;
-    }
-
-    .product-image,
-    .product-info {
-        margin-top: 10px;
-        width: 50%;
-    }
-
-    .color-box {
-        width: 40px;
-        height: 40px;
-        margin-top: 5px;
-    }
-
-    .cart {
-        margin-right: 25px;
-        float: right;
-        border: 1px solid #d8d8d8;
-        padding: 5px 20px;
-    }
-
-    button {
-        margin-top: 30px;
-        border: none;
-        background-color: #1E95EA;
-        color: white;
-        height: 40px;
-        width: 100px;
-        font-size: 14px;
-    }
-
-    .disabledButton {
-        background-color: #d8d8d8;
-    }
-
-    .review-form {
-        width: 400px;
-        padding: 20px;
-        margin: 40px;
-        border: 1px solid #d8d8d8;
-    }
-
-    input {
-        width: 100%;
-        height: 25px;
-        margin-bottom: 20px;
-    }
-
-    textarea {
-        width: 100%;
-        height: 60px;
-    }
-
-    .tab {
-        margin-left: 20px;
-        cursor: pointer;
-    }
-
-    .activeTab {
-        color: #16C0B0;
-        text-decoration: underline;
-    }
-</style>
+<style src="./product_card.css"></style>
