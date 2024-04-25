1b4e51aad5c2763cf9f4983ca077649c8d507b91
Artem Tsepkov <tzepart@gmail.com>
2024-04-25 17:38:27 +0200

    Move Reviews to tabs

---
+++ src/components/ProductReviews.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <p v-if="!reviews.length">There are no reviews yet.</p>
+        <ul>
+            <li v-for="(review, index) in reviews" v-bind:key="index">
+                <p>Author: {{ review.name }}</p>
+                <p>Rating: {{ review.rating }}</p>
+                <p>Comment: {{ review.review }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ProductReviews",
+        props: {
+            reviews: {
+                type: Array,
+                required: true
+            },
+        }
+    }
+</script>
