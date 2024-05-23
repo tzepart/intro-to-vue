58d49caa3ac8f15ed47c56e1ad368c74774d11f5
Artem Tsepkov <tzepart@gmail.com>
2024-05-23 08:31:47 +0200

    Create ProductCreate and ProductList pages + routing rules

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,31 @@
 <template>
     <div id="app">
-        <div class="cart">
-            <p>Cart({{ inCart.length }})</p>
+        <div id="nav">
+            <router-link :to="{ name: 'product-list' }">Product List</router-link> |
+            <router-link :to="{ name: 'product-card' }">Show Product #1</router-link> |
+            <router-link :to="{ name: 'product-create' }">Create Product</router-link>
+            <div class="cart">
+                <p>Cart({{ inCart.length }})</p>
+            </div>
         </div>
-        <ProductCard
-                :premium="premium"
-                @add-to-cart="updateCart"
-                msg="Welcome to Example App"
-        />
+        <router-view/>
     </div>
 </template>
 
 <script>
-    import ProductCard from "./views/ProductCard.vue"
+    import {EventBus} from './main'
 
     export default {
         name: "App",
         data() {
             return {
-                premium: false,
                 inCart: []
             }
         },
-        components: {
-            ProductCard
-        },
-        methods: {
-            updateCart(id) {
-                this.inCart.push(id);
-            }
+        mounted() {
+            EventBus.$on('add-to-cart', inCart => {
+                this.inCart.push(inCart)
+            })
         }
     }
 </script>
@@ -44,5 +41,23 @@
         background: linear-gradient(-90deg, #84CF6A, #16C0B0);
         height: 60px;
         margin-bottom: 15px;
+    }
+
+    .cart {
+        margin-right: 25px;
+        float: right;
+        border: 1px solid #d8d8d8;
+        padding: 5px 20px;
+    }
+
+    #nav {
+        padding: 30px;
+    }
+    #nav a {
+        font-weight: bold;
+        color: #2c3e50;
+    }
+    #nav a.router-link-exact-active {
+        color: #42b983;
     }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import ProductList from '../views/ProductList.vue'
 import ProductCard from '../views/ProductCard.vue'
+import ProductCreate from '../views/ProductCreate.vue'
 
 Vue.use(VueRouter);
 
 const routes = [
     {
-        path: '/',
-        name: 'Home',
+        path: '/product-list',
+        name: 'product-list',
+        component: ProductList,
+        alias: '/'
+    },
+    {
+        path: '/product-card',
+        name: 'product-card',
         component: ProductCard
-    }
+    },
+    {
+        path: '/product-create',
+        name: 'product-create',
+        component: ProductCreate
+    },
 ];
 
 const router = new VueRouter({
