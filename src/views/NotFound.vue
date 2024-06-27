5ce002336f5248126059019973c216072ae7994b
Artem Tsepkov <tzepart@gmail.com>
2024-06-27 11:13:46 +0200

    Not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>
+        Sorry, page not found :(
+    </h1>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
